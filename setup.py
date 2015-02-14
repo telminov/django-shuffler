df0e40df7ed241fb91afdb0513807f6082661760
Telminov <sergey@telminov.ru>
2015-02-14 18:43:26 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='django-shuffler',
-    version='0.01',
+    version='0.02',
     description='Mixing DB data for faking personal data.',
     author='Telminov Sergey',
     url='https://github.com/telminov/shuffler',
